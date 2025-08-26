fd3aa60d06899d733407720c1598c3ec4b950126
TJnotJT <tjnotjt.github@gmail.com>
2025-08-26 06:17:25 -0400

    GS: Make vertex dump YAML. Add comments to output .

--- pcsx2/GS/GSUtil.h
+++ pcsx2/GS/GSUtil.h
@@ -15,6 +15,12 @@ public:
 	static const char* GetWMName(u32 wm);
 	static const char* GetZTSTName(u32 ztst);
 	static const char* GetPrimName(u32 prim);
+	static const char* GetMMAGName(u32 mmag);
+	static const char* GetMMINName(u32 mmin);
+	static const char* GetMTBAName(u32 mtba);
+	static const char* GetLCMName(u32 lcm);
+	static const char* GetSCANMSKName(u32 scanmsk);
+	static const char* GetDATMName(u32 datm);
 
 	static const u32* HasSharedBitsPtr(u32 dpsm);
 	static bool HasSharedBits(u32 spsm, const u32* ptr);
