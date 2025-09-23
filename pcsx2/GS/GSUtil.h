3a1caf58ee70a15d45a5864923c636c194714534
TJnotJT <tjnotjt.github@gmail.com>
2025-09-23 10:59:41 -0400

    optimized right triangle check; thanks to tellowkrinkle.

--- pcsx2/GS/GSUtil.h
+++ pcsx2/GS/GSUtil.h
@@ -5,6 +5,7 @@
 
 #include "GS.h"
 #include "GSRegs.h"
+#include "GS/Renderers/Common/GSVertex.h"
 
 class GSUtil
 {
@@ -72,4 +73,28 @@ public:
 	{
 		return GetClassVertexCount(GetPrimClass(prim));
 	}
+
+	// For returning order of vertices to form a right triangle
+	struct TriangleOrdering
+	{
+		// Describes a right triangle laid out in one of the following orientations
+		// b   c | c  b | a     |     a
+		// a     |    a | b   c | c   b
+		u32 a; // Same x as b
+		u32 b; // Same x as a, same y as c
+		u32 c; // Same y as b
+	};
+
+	// Determines ordering of two triangles in parallel if both are right.
+	// More efficient than calling IsTriangleRight twice.
+	template <u32 tme, u32 fst>
+	static bool AreTrianglesRight(const GSVertex* RESTRICT vin, const u16* index0, const u16* index1,
+		TriangleOrdering* out_triangle0, TriangleOrdering* out_triangle1);
+	// Determines ordering of a single triangle
+	template <u32 tme, u32 fst>
+	static bool IsTriangleRight(const GSVertex* RESTRICT vin, const u16* index, TriangleOrdering* out_triangle);
+	// Determines whether the triangle are right and form a quad
+	template <u32 tme, u32 fst>
+	static bool AreTrianglesQuad(const GSVertex* RESTRICT vin, const u16* index0, const u16* index1,
+		TriangleOrdering* out_triangle0, TriangleOrdering* out_triangle1);
 };
