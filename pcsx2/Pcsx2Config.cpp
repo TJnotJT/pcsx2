d7d6e4ec4e4b39813fd4b3d97af90021e862ed3a
TJnotJT <tjnotjt.github@gmail.com>
2026-01-01 13:24:08 -0500

    GS/DX12/VK: ROV support.

--- pcsx2/Pcsx2Config.cpp
+++ pcsx2/Pcsx2Config.cpp
@@ -752,6 +752,16 @@ Pcsx2Config::GSOptions::GSOptions()
 	PreloadFrameWithGSData = false;
 	Mipmap = true;
 	HWMipmap = true;
+	HWROV = false;
+	HWROVLogging = false;
+	HWROVHistoryTextures = 16;
+	HWROVHistoryDraws = 32;
+	HWROVHistoryWeightColor = 0.75f;
+	HWROVHistoryWeightDepth = 0.75f;
+	HWROVBarriersEnableColor = 2.0f;
+	HWROVBarriersEnableDepth = 4.0f;
+	HWROVBarriersDisableColor = 1.125f;
+	HWROVBarriersDisableDepth = 1.25f;
 
 	ManualUserHacks = false;
 	UserHacks_AlignSpriteX = false;
@@ -1024,6 +1034,16 @@ void Pcsx2Config::GSOptions::LoadSave(SettingsWrapper& wrap)
 	SettingsWrapEntryEx(UpscaleMultiplier, "upscale_multiplier");
 
 	SettingsWrapBitBoolEx(HWMipmap, "hw_mipmap");
+	SettingsWrapBitBool(HWROV);
+	SettingsWrapBitBool(HWROVLogging);
+	SettingsWrapEntry(HWROVHistoryDraws);
+	SettingsWrapEntry(HWROVHistoryTextures);
+	SettingsWrapEntry(HWROVHistoryWeightColor);
+	SettingsWrapEntry(HWROVHistoryWeightDepth);
+	SettingsWrapEntry(HWROVBarriersEnableColor);
+	SettingsWrapEntry(HWROVBarriersEnableDepth);
+	SettingsWrapEntry(HWROVBarriersDisableColor);
+	SettingsWrapEntry(HWROVBarriersDisableDepth);
 	SettingsWrapIntEnumEx(AccurateBlendingUnit, "accurate_blending_unit");
 	SettingsWrapIntEnumEx(TextureFiltering, "filter");
 	SettingsWrapIntEnumEx(TexturePreloading, "texture_preloading");
