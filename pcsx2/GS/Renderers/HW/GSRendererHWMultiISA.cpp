bc59bb14bb618f54f2b822fbcdcf4cae579538c2
TJnotJT <tjnotjt.github@gmail.com>
2026-03-10 20:32:56 -0400

    Mirror SW renderer UV rounding setup in HW renderer SW draw.

--- pcsx2/GS/Renderers/HW/GSRendererHWMultiISA.cpp
+++ pcsx2/GS/Renderers/HW/GSRendererHWMultiISA.cpp
@@ -40,6 +40,7 @@ bool GSRendererHWFunctions::SwPrimRender(GSRendererHW& hw, bool invalidate_tc, b
 	GSScanlineGlobalData& gd = data.global;
 
 	const u32 round_uv = static_cast<u32>(hw.GetVertexUVRoundingInfo());
+	const u32 fst = PRIM->FST | round_uv; // UV rounding pre-divides ST by Q and saves as UVs.
 
 	hw.m_sw_vertex_buffer.resize(((hw.m_vertex.next + 1) & ~1));
 
@@ -55,7 +56,7 @@ bool GSRendererHWFunctions::SwPrimRender(GSRendererHW& hw, bool invalidate_tc, b
 	// Optimize the division by 1 with a nop. It also means that GS_SPRITE_CLASS must be processed when !vt.m_eq.q.
 	// If you have both GS_SPRITE_CLASS && vt.m_eq.q, it will depends on the first part of the 'OR'.
 	const u32 q_div = !hw.IsMipMapActive() && ((vt.m_eq.q && vt.m_min.t.z != 1.0f) || (!vt.m_eq.q && vt.m_primclass == GS_SPRITE_CLASS));
-	GSVertexSW::s_cvb[vt.m_primclass][PRIM->TME][PRIM->FST][q_div][round_uv](
+	GSVertexSW::s_cvb[vt.m_primclass][PRIM->TME][fst][q_div][round_uv](
 		context, data.vertex, hw.m_vertex.buff, hw.m_vertex.next);
 
 	GSVector4i scissor = context->scissor.in;
@@ -90,8 +91,6 @@ bool GSRendererHWFunctions::SwPrimRender(GSRendererHW& hw, bool invalidate_tc, b
 	gd.sel.tfx = TFX_NONE;
 	gd.sel.ababcd = 0xff;
 	gd.sel.prim = primclass;
-
-	gd.sel.rounduv = !!round_uv;
 
 	u32 fm = context->FRAME.FBMSK;
 	u32 zm = context->ZBUF.ZMSK || context->TEST.ZTE == 0 ? 0xffffffff : 0;
@@ -167,8 +166,9 @@ bool GSRendererHWFunctions::SwPrimRender(GSRendererHW& hw, bool invalidate_tc, b
 		{
 			gd.sel.tfx = context->TEX0.TFX;
 			gd.sel.tcc = context->TEX0.TCC;
-			gd.sel.fst = PRIM->FST;
+			gd.sel.fst = PRIM->FST | round_uv;
 			gd.sel.ltf = vt.IsLinear();
+			gd.sel.rounduv = round_uv;
 
 			if (GSLocalMemory::m_psm[context->TEX0.PSM].pal > 0)
 			{
